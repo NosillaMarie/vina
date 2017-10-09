d2f6efae166c91affa5e79d4c9d2be2e454a087e
ncevallos <nmcevall@gmail.com>
2017-10-09 11:05:52 -0400

    logic for quiz in place

--- appLogic/assets/javascript/app.js
+++ appLogic/assets/javascript/app.js
@@ -1,4 +1,4 @@
-var quizlength = 7;
+var quizlength = 8;
 var userAnswers = [];
 var action = 0;
 var adventure =0;
@@ -47,18 +47,23 @@ var questionNumber = 0;
         console.log("statement is equal to "+statement);
         switch(statement){
           case 'A':
+            fantasy += 1;
             console.log("User selected A");
             break;
           case 'B':
+            action += 1;
             console.log("User selected B");
             break;
           case 'C':
+            fantasy += 1;
             console.log("User selected C");
             break;
           case 'D':
+            thriller += 1;
             console.log("User selected D");
             break;
           case 'E':
+            documentary += 1;
             console.log("User selected E");
             break;
         }
@@ -67,18 +72,25 @@ var questionNumber = 0;
         console.log("block 2");
         switch(statement){
           case 'A':
+            drama += 2;
             console.log("User selected A");
             break;
           case 'B':
+            fantasy += 1;
+            action += 2;
             console.log("User selected B");
             break;
           case 'C':
+            fantasy += 2;
+            drama += 1;
             console.log("User selected C");
             break;
           case 'D':
+            scifi += 1;
             console.log("User selected D");
             break;
           case 'E':
+            action += 1;
             console.log("User selected E");
             break;
         }
@@ -89,18 +101,23 @@ var questionNumber = 0;
         console.log("block 3");
         switch(statement){
           case 'A':
+            action += 3;
             console.log("User selected A");
             break;
           case 'B':
+            adventure += 3;
             console.log("User selected B");
             break;
           case 'C':
+            romantic += 3;
             console.log("User selected C");
             break;
           case 'D':
+            thriller += 3;
             console.log("User selected D");
             break;
           case 'E':
+            comedy += 3;
             console.log("User selected E");
             break;
         }
@@ -111,18 +128,24 @@ var questionNumber = 0;
         console.log("block 4");
         switch(statement){
           case 'A':
+            comedy += 2;
+            drama += 2;
             console.log("User selected A");
             break;
           case 'B':
+            drama += 2;
             console.log("User selected B");
             break;
           case 'C':
+            romantic += 3;
             console.log("User selected C");
             break;
           case 'D':
+            thriller += 3;
             console.log("User selected D");
             break;
           case 'E':
+            documentary += 1;
             console.log("User selected E");
             break;
         }
@@ -133,18 +156,25 @@ var questionNumber = 0;
         console.log("block 5");
         switch(statement){
           case 'A':
+            action += 3;
             console.log("User selected A");
             break;
           case 'B':
+            drama += 3;
             console.log("User selected B");
             break;
           case 'C':
+            documentary += 1;
+            adventure += 2;
             console.log("User selected C");
             break;
           case 'D':
+            action += 3;
             console.log("User selected D");
             break;
           case 'E':
+            drama += 2;
+            documentary += 2;
             console.log("User selected E");
             break;
         }
@@ -155,18 +185,28 @@ var questionNumber = 0;
         console.log("block 6");
         switch(statement){
           case 'A':
+            adventure += 3;
+            action += 1;
             console.log("User selected A");
             break;
           case 'B':
+            action += 3;
+            adventure += 1;
             console.log("User selected B");
             break;
           case 'C':
+            drama += 2;
+            romantic += 3;
+            comedy += 1;
             console.log("User selected C");
             break;
           case 'D':
+            documentary += 3;
+            drama += 1;
             console.log("User selected D");
             break;
           case 'E':
+            comedy += 4;
             console.log("User selected E");
             break;
         }
@@ -177,18 +217,54 @@ var questionNumber = 0;
         console.log("block 7");
         switch(statement){
           case 'A':
+            scifi += 3;
+            fantasy += 3;
             console.log("User selected A");
             break;
           case 'B':
+            fantasy += 3;
+            adventure += 3;
             console.log("User selected B");
             break;
           case 'C':
+            action += 3;
+            adventure += 3;
             console.log("User selected C");
             break;
           case 'D':
+            documentary += 3;
+            drama += 1;
             console.log("User selected D");
             break;
           case 'E':
+            romantic += 3;
+            drama += 1;
+            console.log("User selected E");
+            break;
+        }
+       else if(question === 8){
+        console.log("block 3");
+        switch(statement){
+          case 'A':
+            action += 1;
+            console.log("User selected A");
+            break;
+          case 'B':
+            adventure += 1;
+            console.log("User selected B");
+            break;
+          case 'C':
+            drama += 1;
+            console.log("User selected C");
+            break;
+          case 'D':
+            fantasy += 1;
+            scifi += 3;
+            console.log("User selected D");
+            break;
+          case 'E':
+            documentary += 2;
+            drama += 1;
             console.log("User selected E");
             break;
         }
