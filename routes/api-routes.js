22353fb34be0c665e9c8afe9b16b382ecf74f6bf
Allison Marie <aliederman@icloud.com>
2017-10-10 21:28:35 -0400

    we are listening on port 8080

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,21 @@
 var db = require("../models");
 
-// POST route for new user
-app.post("/api/add", function (req, res) {
+module.exports = function (app) {
 
-    console.log(req.body);
+    // POST route for new user
+    app.post("/api/add", function (req, res) {
 
-    db.Users.create({
-        email: req.body.email,
-        topGenre: req.body.topGenre,
-        secondGenre: req.body.secondGenre,
-        firebase_ID: req.body.firebaseID
-    }).then(function (results) {
-        // `results` here would be the newly created chirp
-        res.redirect("/");
+        console.log(req.body);
+
+        db.Users.create({
+            email: req.body.email,
+            topGenre: req.body.topGenre,
+            secondGenre: req.body.secondGenre,
+            firebase_ID: req.body.firebaseID
+        }).then(function (results) {
+            // `results` here would be the newly created chirp
+            res.redirect("/");
+        });
+
     });
-
-});
-
 };
