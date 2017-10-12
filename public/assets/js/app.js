5774be78a46eb49065a49f7c3f60a08b07d95e92
Allison Marie <aliederman@icloud.com>
2017-10-12 09:06:47 -0400

    adding picks for user code - not working right - Vitek please check

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -29,31 +29,31 @@
 
     });
 
-    //    btnSignUp.addEventListener('click', e => {
-    //
-    //        const email = txtEmail.value;
-    //        const pass = txtPassword.value;
-    //        const auth = firebase.auth();
-    //
-    //        const promise = auth.createUserWithEmailAndPassword(email, pass);
-    //
-    //        promise.catch(e => console.log(e.message));
-    //
-    //    });
+    btnSignUp.addEventListener('click', e => {
 
-    //    btnLogout.addEventListener('click', e => {
-    //        firebase.auth().signOut();
-    //    });
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
 
-    //    firebase.auth().onAuthStateChanged(firebaseUser => {
-    //        if (firebaseUser) {
-    //            console.log(firebaseUser);
-    //            btnLogout.classList.remove('hide');
-    //        } else {
-    //            console.log("Not Logged In");
-    //            btnLogout.classList.add('hide');
-    //        }
-    //    });
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+
+        promise.catch(e => console.log(e.message));
+
+    });
+
+    //        btnLogout.addEventListener('click', e => {
+    //            firebase.auth().signOut();
+    //        });
+
+    //        firebase.auth().onAuthStateChanged(firebaseUser => {
+    //            if (firebaseUser) {
+    //                console.log(firebaseUser);
+    //                btnLogout.classList.remove('hide');
+    //            } else {
+    //                console.log("Not Logged In");
+    //                btnLogout.classList.add('hide');
+    //            }
+    //        });
 
 }());
 
@@ -79,7 +79,80 @@ $(document).ready(function () {
 var user = firebase.auth().currentUser;
 var uid, email;
 
-if (user != null) {
+if (user !== null) {
     email = user.email;
     uid = user.uid;
 }
+
+$(document).ready(function () {
+
+    //function for inserting DB rows into HTML
+
+    function loadUsers(table) {
+
+        var $users = $('#usersTable');
+
+        for (i = 0; i < table.length; i++) { //loop through JSON with db data and append to $('#users-container')
+
+            $users.append(
+
+                "<tr><td>" + table[i].email + "</td>" +
+                "<td>" + table[i].topGenre + "</td>" +
+                "<td>" + table[i].secondGenre + "</td>" +
+                "<td>" + "FIREBASE ID" + "</td>"
+            );
+        }
+    }
+
+    //populate all users from DB with get request
+    $.get("/api/users", function (data) {
+
+        console.log(data);
+
+        loadUsers(data);
+
+    });
+
+    //populate genre selections
+    var queryURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=3d866c05691ba06f9fa697f8e8c9e838&language=en-US";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        var genres = response.genres;
+        for (i = 0; i < genres.length; i++) {
+
+            $('select').append(
+
+                "<option value='" + genres[i].id + "'>" + genres[i].name + "</option>"
+            );
+        };
+    });
+
+    //create new user
+
+    $('#submit').click(function () {
+
+        event.preventDefault();
+
+        var $email = $('.new-email'); //email field
+        var genreOne = $('#genreOne').val();
+        var genreTwo = $('#genreTwo').val();
+
+        var newUser = {
+            email: $email.val().trim(),
+            topGenre: genreOne,
+            secondGenre: genreTwo
+        };
+
+        $.post("/api/add", newUser)
+            .done(function () {
+                $email.val("");
+                $('#user-pass').val("");
+                location.reload();
+            });
+
+    });
+
+});
